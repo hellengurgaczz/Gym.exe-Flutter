3ee12f65ee9935ff6fc14903f63629b1871eec6f
hellen <gurgaczzhellen@gmail.com>
2022-10-21 17:48:50 -0300

    realizada conexao com banco de dados back4app

--- Ticket4U/lib/main.dart
+++ Ticket4U/lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 import './home_screen.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 
-void main() {
+Future<void> main() async {
+  final keyApplicationId = 'Blhx5C01V0fq73wDrWm5fFKpu9e1HecPNl8yWIdS';
+  final keyClientKey = 'uuyBdaQnwEq0TxCvNYYGfJLpzwtCTMN6BpyPSRRV';
+  final keyParseServerUrl = 'https://parseapi.back4app.com';
+
+  await Parse().initialize(keyApplicationId, keyParseServerUrl,
+      clientKey: keyClientKey, autoSendSessionId: true);
+
+  var firstObject = ParseObject('Mensagem')
+    ..set('texto', 'Hey ! First message from Flutter. Parse is now connected');
+  await firstObject.save();
   runApp(const MyApp());
 }
 
